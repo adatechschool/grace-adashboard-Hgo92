b58dcf9e3a17f6989c7908a219d3f230e86c0671
Hgo92 <hugo_monier@yahoo.fr>
2026-01-14 13:53:18 +0100

    transition pc portable-pc fixe

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,49 @@ export default function App() {
     }
   };
 
+  // Fonctionne pas
+  const addTheme = async (id) => {
+    try {
+    await fetch(`http://localhost:3000/themes/${id}`, {
+      method:"POST"
+    }); 
+  setData(prevData => prevData.filter(theme => theme.id !== id));}
+    catch (error) {
+      console.error(error)
+    }
+  };
+
   const resetProgress = () => {
   localStorage.clear();
   window.location.reload();
 };
+
+// Fonctionne pas
+const [skills, setSkills] = useState(["", "", ""]);
+const addSkill = () => (
+    setSkills(prevSkills => [...prevSkills, ""])
+  )
   
   return (
     <div><h1>Mes compétences</h1>
     <button onClick={resetProgress}>Réinitialiser ma progression</button>
+    <button onClick={addTheme}>Ajouter un thème</button>
+    <div>
+      <div>
+        <h3>Thème</h3>
+        <input type ="text" placeholder="Nom du thème"></input>
+        <h3>Compétence</h3>
+        {skills.map((skill, index) => (
+          <input
+            key={index}
+            type="text"
+            placeholder="Insérez une compétence"
+          />
+        ))}
+        <button onClick={addSkill}>Ajouter une autre compétence</button>
+        <button>Valider</button>
+      </div>
+    </div>
     <BlockThemes
       listThemes = {data}
       onRemoveTheme = {removeTheme}
